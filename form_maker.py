260cd19e788cd0d15906af5dc3004c733b4784a6
Dustin Roan <dar5543@truman.edu>
2018-07-30 22:24:22 -0500

    Added more pydoc info

--- form_maker.py
+++ form_maker.py
@@ -8,6 +8,7 @@ import json
 def __load_file(file_name):
     """Loads a json file
 
+    file_name: string: the name of the json file
     """
     read_in = open(file_name, 'r')
     return json.load(read_in)
@@ -16,15 +17,19 @@ def __load_file(file_name):
 def __generate_label(text):
     """Generates a label with a given text as the innerHTML
 
+    text: string: the text to be shown in a label
     """
     return "<label>" + text + "</label>\n"
 
 
-def __generate_input_type(value, attr_list, attr_vals):
+def __generate_input_type(type_of, attr_list, attr_vals):
     """Generates an input tag based on given information
 
+    type_of: string: name of the input type
+    attr_list: list: list of all the attributes to add to the input tag
+    attr_vals: list: list of all of the values of the attribues to add to input tag
     """
-    output = '<input type="' + value + '"'
+    output = '<input type="' + type_of + '"'
     for i in range(len(attr_list)):
         output += __generate_input_attr(attr_list[i], attr_vals[i])
 
@@ -36,20 +41,21 @@ def __generate_input_type(value, attr_list, attr_vals):
 def __generate_input_attr(name, value):
     """Generates an html tag attribute and value
 
+    name: string: the name of html input tag attribute
+    value: string: the value associated with an html input tag attribute
     """
     return name + '="' + value + '" '
 
 
 def __generate_break():
-    """Creates a html break tag
-
-    """
+    """Creates a html break tag"""
     return "<br>"
 
 
 def generate_html(element):
     """Generates html based on type
 
+    element: dict: dictionary containing information about an input tag
     """
     try:
         type_of = element["supertype"]
@@ -67,6 +73,7 @@ def generate_html(element):
 def generate_text_type(element):
     """Generates text based non-choice input items
 
+    element: dict: dictionary containing information about an input tag
     """
     output = __generate_label(element["label"])
     attributes = element["attributes"]
@@ -92,6 +99,7 @@ def generate_text_type(element):
 def generate_choice(element):
     """Generates radio button inputs
 
+    element: dict: dictionary containing information about an input tag
     """
     output = __generate_label(element["label"]) + __generate_break()
     output += "<fieldset>"
